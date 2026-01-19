3433a641e17be1ef1015e1a10ac1f62663c86bcc
Ariadne <ariadnevk21@gmail.com>
2026-01-19 20:00:22 +1100

    beginning :)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// File contains vite's configuration for the project.
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
